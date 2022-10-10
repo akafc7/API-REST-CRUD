7efcfbaa50dd216409245fc4edfb55b839ed5ca9
Fernando Cavanellas <fernandocavanellas6@hotmail.com>
2022-10-10 17:03:12 -0300

    ending CRUD

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -2,15 +2,18 @@ const router = require('express').Router();
 
 const Person = require('../models/Person');
 
+// Criação de dados - CREATE
 router.post('/', async (req, res) => {
     // req.body
     const { name, salary, approved } = req.body;
 
     if (!name) {
         res.status(422).json({ error: 'O nome é obrigatório!' });
+        return;
     }
     if (!salary) {
         res.status(422).json({ error: 'O salário é obrigatório!' });
+        return;
     }
 
     const person = {
@@ -26,6 +29,81 @@ router.post('/', async (req, res) => {
         res.status(201).json({
             msg: 'Pessoa inserida no sistema com sucesso!',
         });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+// READ - Leitura de Dados
+router.get('/', async (req, res) => {
+    try {
+        const people = await Person.find();
+
+        res.status(200).json(people);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    // extrair o dado da requisição, pela url = req.params
+    const id = req.params.id;
+
+    try {
+        const person = await Person.findOne({ _id: id });
+
+        if (!person) {
+            res.status(424).json({ msg: 'O usuário não foi encontrado!' });
+            return;
+        }
+
+        res.status(200).json(person);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+// UPDATE - atualização de dados (PUT, PATCH)
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const { name, salary, approved } = req.body;
+
+    const person = {
+        name,
+        salary,
+        approved,
+    };
+
+    try {
+        const updatedPerson = await Person.updateOne({ _id: id }, person);
+
+        if (updatedPerson.matchedCount === 0) {
+            res.status(422).json({ msg: 'Usuário não encontrado!' });
+            return;
+        }
+
+        res.status(200).json(person);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
+// DELETE - deletar dados
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const person = await Person.findOne({ _id: id });
+
+    if (!person) {
+        res.status(424).json({ msg: 'O usuário não foi encontrado!' });
+        return;
+    }
+
+    try {
+        await Person.deleteOne({ _id: id });
+
+        res.status(200).json({ msg: 'Usuário removido com sucesso!' });
     } catch (error) {
         res.status(500).json({ error: error });
     }
